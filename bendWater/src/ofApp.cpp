78fe50e5d5f3bb7e7bb922ea01c8558106f4e609
figgyfunk <figa@rpi.edu>
2017-10-26 18:12:19 -0400

    added scaling. not gonna lie it took a really long time to get it looking good and how i wanted it to. it might've been a mistake to mess with shaders BUT I don't care.

--- bendWater/src/ofApp.cpp
+++ bendWater/src/ofApp.cpp
@@ -2,7 +2,7 @@
 ofMolecule m;
 ofMolecule m2;
 ofShader shader;
-ofShader shader2;
+
 
 
 ofMolecule::ofMolecule() {
@@ -23,36 +23,21 @@ ofMolecule::ofMolecule(ofVec2f p, ofShader shader) {
 	vel = ofVec2f(0, 0);
 	vel2 = ofVec2f(0, 0);
 	vel3 = ofVec2f(0, 0);
-	drag = ofRandom(.6, .7);
+	
 	shad = shader;
 	
 }
 void ofMolecule::update() {
-	
+	drag = ofRandom(.90, .99);
 	ofVec2f mouse(ofGetMouseX(), ofGetMouseY());
 	
 	frc = mouse - pos;
-	ofVec2f frc2 = mouse - pos2;
-	ofVec2f frc3 = mouse - pos3;
-
 	float dist = frc.length();
-	float dist2 = frc2.length();
-	float dist3 = frc3.length();
 	frc.normalize();
-	frc2.normalize();
-	frc3.normalize();
-
 	vel *= drag;
-	vel += frc * 0.6;
-	vel2 *= (drag*.95);
-	vel2 += frc2 * 0.7;
-	vel3 *= (drag*.95);
-	vel3 += frc3 * 0.75;
-	shad.setUniform2f("mouse", mouse);
-	
+	vel += frc * 0.4;
 	pos += vel;
-	pos2 += vel2;
-	pos3 += vel3;
+	
 
 	//keep in screen
 	if (pos.x > ofGetWidth()) {
@@ -78,10 +63,10 @@ void ofMolecule::draw() {
 	shad.begin();
 	shad.setUniform1f("timeValX", ofGetElapsedTimef() * 0.15);
 	shad.setUniform1f("timeValY", -ofGetElapsedTimef() * 0.23);
-	shad.setUniform1f("drag", ofRandom(.2, .98));
+	shad.setUniform1f("drag", ofRandom(.96, .98));
+	shad.setUniform3f("center", ofVec3f(pos.x, pos.y, 1));
+	shad.setUniform2f("mouse", ofVec2f(ofGetMouseX(), ofGetMouseY()));
 	ofDrawSphere(pos.x, pos.y, 50);
-	ofDrawSphere(pos2.x, pos2.y, 40);
-	ofDrawSphere(pos3.x, pos3.y, 35);
 	shad.end();
 	
 
@@ -90,37 +75,45 @@ void ofMolecule::draw() {
 void ofApp::setup(){
 	
 	ofSetBackgroundAuto(false);
-	ofBackground(0, 0, 0);
+	//ofBackground(ofColor().white);
 	ofEnableAlphaBlending();
 	ofSetFrameRate(150);
 	shader.load("shaders/shader");
+
 	m = ofMolecule(ofVec2f(0,0),shader);
-	m2 = ofMolecule(ofVec2f(ofGetWidth(), ofGetHeight()), shader);
-	count = 0;
+	//m2 = ofMolecule(ofVec2f(ofGetWidth(), ofGetHeight()), shader);
+
 	 
 	 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	
 	count += 1;
 	m.update();
-	m2.update();
+	//m2.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-	if (count % 1 == 0) {
+	if (count % 2 == 0) {
 		ofSetColor(255, 255, 255, 3);
 		ofRect(0, 0, ofGetWidth(), ofGetHeight());
 	}
 	
 	m.draw();
-	m2.draw();
-	
-
-	
+	//m2.draw();
+	/*ofSetColor(ofColor().black);
+	shader.begin();
+	shader.setUniform1f("timeValX", ofGetElapsedTimef() * 0.15);
+	shader.setUniform1f("timeValY", -ofGetElapsedTimef() * 0.23);
+	shader.setUniform1f("drag", ofRandom(.2, .98));
+	shader.setUniform2f("mouse", ofVec2f(ofGetMouseX(), ofGetMouseY()));
+	shader.setUniform3f("center", ofVec3f(ofGetWidth() / 2, ofGetHeight() / 2, 1));
+	ofDrawSphere(ofGetWidth()/2, ofGetHeight()/2, 1, 50);
+	shader.end();*/
 }
 
 //--------------------------------------------------------------
