b7fa97e8ac43fb6b7771c1d9f7f4eb6efe4fbdcb
figgyfunk <figa@rpi.edu>
2017-10-30 22:26:41 -0400

    DEAR GOD MY COMPUTER TOOK LIKE 6 HOURS TO UPDATE. Good news is that after suffering and getting it set up on this computer it does run faster.

--- bendWater/src/ofApp.cpp
+++ bendWater/src/ofApp.cpp
@@ -186,18 +186,14 @@ void ofApp::draw(){
 		//filter2.draw(camWidth + 320, 240, 320, 240);
 		//finalImage.draw(0, camHeight, 320, 240);
 	}
-	contours.draw(0, 0);
 
-	ofNoFill();
+
+
 	//printf("size of blobs: %d", finder.blobs.size());
 	if (finder.blobs.size() > 0) {
 		m.draw(finder.blobs[0].centroid);
 	}
-	for (int i = 0; i < finder.blobs.size(); ++i) {
-		ofDrawRectangle(finder.blobs[i].boundingRect);
 
-	}
-	ofFill();
 }
 
 //--------------------------------------------------------------
