701083a81903075fe231f47d5cc81655efc071f0
figgyfunk <figa@rpi.edu>
2017-11-06 15:50:43 -0500

    changed so program is size of camera.

--- bendWater/src/main.cpp
+++ bendWater/src/main.cpp
@@ -5,8 +5,8 @@
 int main( ){
 	ofGLFWWindowSettings settings;
 	settings.setGLVersion(3, 2); //we define the OpenGL version we want to use
-	settings.width = 1024;
-	settings.height = 768;
+	settings.width = 640;
+	settings.height = 480;
 	ofCreateWindow(settings);
 	// this kicks off the running of my app
 	ofRunApp(new ofApp());
